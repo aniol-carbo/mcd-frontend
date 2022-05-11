a767a8ec839beb947e64589c4eafcb1016e407a3
aniol-carbo <aniol.carbo@estudiantat.upc.edu>
2022-05-11 18:48:55 +0200

    Fix CORS error by adding 'http://' to the URL

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -7,7 +7,7 @@ export class DbService {
   constructor(private http: HttpClient) { }
 
   getItems(status: string, startDate: number, endDate: number, orderBy: string): Promise<any>{
-    return this.http.get(`localhost:3000/api/db/sold?status=${status}&orderBy=${orderBy}&startDate=${startDate}&endDate=${endDate}`).
+    return this.http.get(`http://localhost:3000/api/db/${status}?orderBy=${orderBy}&startDate=${startDate}&endDate=${endDate}`).
     toPromise();
   }
 
